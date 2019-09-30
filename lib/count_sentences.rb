b9dd00995a4fb4da2a0f13952b900ee914b029cc
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-30 05:37:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+  "Hi, my name is Sophie."
+    self.end_with?(".")
   end
 
   def question?
-
+    "Happy Halloween!"
+self.end_with?("?")
   end
 
   def exclamation?
-
+    "Hi, my name is Sophie"
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    binding.pry
+    "one. two. three. four!"
+    self.split(".").count
+  end
   end
 end
